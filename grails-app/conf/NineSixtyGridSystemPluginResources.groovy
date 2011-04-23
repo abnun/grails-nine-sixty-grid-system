ff3c17d3db856b263d2e6d1169abb966698f5ef9
Markus <abnun@gmx.de>
2011-04-23 20:48:30 +0200

    changed plugin id

--- grails-app/conf/NineSixtyGridSystemPluginResources.groovy
+++ grails-app/conf/NineSixtyGridSystemPluginResources.groovy
@@ -1,11 +1,11 @@
 // Define the resources for exposure via Resources plugin
 modules = {
 	'nine-sixty-grid-system' {
-		resource id: 'reset', url: [plugin: 'nine-sixty-grid-system', dir: 'css/nine-sixty-grid-system', file: 'reset.css'],
+		resource id: 'reset', url: [plugin: 'nineSixtyGridSystem', dir: 'css/nine-sixty-grid-system', file: 'reset.css'],
 				attrs: [media: 'all']
-		resource id: 'text', url: [plugin: 'nine-sixty-grid-system', dir: 'css/nine-sixty-grid-system', file: 'text.css'],
+		resource id: 'text', url: [plugin: 'nineSixtyGridSystem', dir: 'css/nine-sixty-grid-system', file: 'text.css'],
 				attrs: [media: 'all']
-		resource id: '960', url: [plugin: 'nine-sixty-grid-system', dir: 'css/nine-sixty-grid-system', file: '960.css'],
+		resource id: '960', url: [plugin: 'nineSixtyGridSystem', dir: 'css/nine-sixty-grid-system', file: '960.css'],
 				attrs: [media: 'all']
 	}
 }
